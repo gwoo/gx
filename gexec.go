766028f1ac28ab5ff89af9241a31e1c1e8475295
gwoo <gwoohoo@gmail.com>
2013-07-16 13:42:35 -0700

    More elegant AuthHandler. Added "/encode" for writing scripts.

--- gexec.go
+++ gexec.go
@@ -19,11 +19,6 @@ var username = flag.String("username", "demo", "Username for basic auth.")
 var password = flag.String("password", "test", "Password for basic auth.")
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	a := AuthHandler(w,r)
-	if !a {
-		fmt.Fprintf(w, "%s", "Not Authorized.")
-		return
-	}
 	src := r.URL.Path[len("/"):]
 	command, err := Save(src)
 	if err != nil {
@@ -66,48 +61,75 @@ func Exec(command string) (string, error) {
 	return string(output), nil
 }
 
-func AuthHandler(w http.ResponseWriter, r *http.Request) bool {
-	url := r.URL
-	for k, v := range r.Header {
-		fmt.Printf("  %s = %s\n", k, v[0])
+func EncodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,
+		"<html><title>Encode a Script</title>"+
+		"<body style='font-family: Monospace;'>")
+
+	body := r.FormValue("body")
+	if body != "" {
+		fmt.Fprintf(w,
+			"<h2>Done. <a href='%s'>Click Here</a></h2>",
+			base64.URLEncoding.EncodeToString([]byte(body)))
 	}
-	auth, ok := r.Header["Authorization"]
-	if !ok {
+    fmt.Fprintf(w,
+		"<h2>Encode</h2>"+
+        "<form action=\"/encode\" method=\"POST\">"+
+        "<p><textarea name=\"body\" style='height:200px;width:400px'>%s</textarea></p>"+
+        "<p><input type=\"submit\" value=\"Submit\"></p>"+
+        "</form>", body)
+    fmt.Fprintf(w, "</body></html>")
+}
+
+func AuthHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL
+		for k, v := range r.Header {
+			fmt.Printf("  %s = %s\n", k, v[0])
+		}
+		auth, ok := r.Header["Authorization"]
+		if !ok {
+			log.Printf("Unauthorized access to %s", url)
+			w.Header().Add("WWW-Authenticate", fmt.Sprintf("basic realm=\"%s\"", *host))
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintf(w, "Not Authorized.")
+			return
+		}
+		encoded := strings.Split(auth[0], " ")
+		if len(encoded) != 2 || encoded[0] != "Basic" {
+			log.Printf("Strange Authorization %q", auth)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		decoded, err := base64.StdEncoding.DecodeString(encoded[1])
+		if err != nil {
+			log.Printf("Cannot decode %q: %s", auth, err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		parts := strings.Split(string(decoded), ":")
+		if len(parts) != 2 {
+			log.Printf("Unknown format for credentials %q", decoded)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if parts[0] == *username && parts[1] == *password {
+			fn(w, r)
+			return
+		}
+		log.Printf("Unauthorized access to %s", url)
 		w.Header().Add("WWW-Authenticate", fmt.Sprintf("basic realm=\"%s\"", *host))
 		w.WriteHeader(http.StatusUnauthorized)
-		log.Printf("Unauthorized access to %s", url)
-		return false
+		fmt.Fprintf(w, "Not Authorized.")
+		return
 	}
-	encoded := strings.Split(auth[0], " ")
-	if len(encoded) != 2 || encoded[0] != "Basic" {
-		log.Printf("Strange Authorizatoion %q", auth)
-		w.WriteHeader(http.StatusBadRequest)
-		return false
-	}
-	decoded, err := base64.StdEncoding.DecodeString(encoded[1])
-	if err != nil {
-		log.Printf("Cannot decode %q: %s", auth, err)
-		w.WriteHeader(http.StatusBadRequest)
-		return false
-	}
-	parts := strings.Split(string(decoded), ":")
-	if len(parts) != 2 {
-		log.Printf("Unknown format for credentials %q", decoded)
-		w.WriteHeader(http.StatusBadRequest)
-		return false
-	}
-	if parts[0] == *username && parts[1] == *password {
-		return true
-	}
-	w.Header().Add("WWW-Authenticate", fmt.Sprintf("basic realm=\"%s\"", *host))
-	w.WriteHeader(http.StatusUnauthorized)
-	log.Printf("Unauthorized access to %s", url)
-	return false
 }
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/favicon.ico", http.NotFound)
+	http.HandleFunc("/", AuthHandler(Handler))
+	http.HandleFunc("/encode", AuthHandler(EncodeHandler))
 	log.Printf("Connected to %s:%d", *host, *port)
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
